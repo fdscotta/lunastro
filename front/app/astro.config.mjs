2560036c6775d885f7f4d79ad36145f56fb8f4b7
Fran <francisco@moka.tv>
2024-04-26 08:49:01 -0300

    first upload

--- front/app/astro.config.mjs
+++ front/app/astro.config.mjs
@@ -1,9 +1,13 @@
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://torneos-manager-f.vercel.app/",
   integrations: [icon()],
-  output: 'static',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
